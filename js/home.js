6f8c98b2104c869e9096ffecabc71664039890ae
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2019-08-17 02:56:15 +0530

    finished styling of timetable, must add db support

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 console.log("main.js")
-function showHome(){
+
+function showHome() {
     var home = document.getElementById("home");
     home.style.display = "flex";
 
@@ -8,7 +9,6 @@ function showHome(){
     var abt = document.getElementById("abt");
     abt.style.display = "none";
 }
-
 function showInfo() {
     var info = document.getElementById("info");
     info.style.display = "flex";
@@ -26,4 +26,21 @@ function showAbt() {
     home.style.display = "none";
     var info = document.getElementById("info");
     info.style.display = "none";
+}
+
+var block = document.getElementsByClassName("block");
+for (var i = 0; i < block.length; i++) {
+    block[i].addEventListener("click", bindClick(i));
+}
+
+function bindClick(i) {
+    return function () {
+        var hexString = document.createElement('div')
+        hexString.style.backgroundColor = `#222`
+        if (block[i].style.backgroundColor === hexString.style.backgroundColor) {
+            block[i].style.backgroundColor = 'aqua';
+        } else {
+            block[i].style.backgroundColor = '#222';
+        }
+    };
 }
